f183615f213ac38aa63b7fad0d119738c4ca95fb
vladgordey <vladgordey@gmail.com>
2017-02-13 23:16:12 -0500

    Update var_elb.tf

--- var_elb.tf
+++ var_elb.tf
@@ -5,16 +5,4 @@
 #
 #
 variable "sec_group" {}
-variable "ami_id" {
-  description = "The AMI to use with the launch configuration"
-}
-variable "instance_type" {}
-variable "iam_instance_profile" {
-  description = "The IAM role the launched instance will use"
-}
-variable "key_name" {
-  description = "The SSH public key name (in EC2 key-pairs) to be injected into instances"
-}
-variable "security_group" {
-  description = "ID of SG the launched instance will use"
-}
+variable "subnets" {}
